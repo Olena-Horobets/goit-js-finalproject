79e7576a5427d8be76d859a204140c39a3900b31
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-01-06 20:04:31 -0330

    lightbox gallery added

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const renderGallery = () => {
+	return galleryItems
+		.map((el) => {
+			return `<li class="gallery__item">
+                    <a class="gallery__link" href="${el.original}">
+                        <img src="${el.preview}"
+                            src="${el.original}"
+                            alt="${el.description}"
+                            class="gallery__image"/>
+                    </a>
+                </li>`;
+		})
+		.join("");
+};
+
+list.innerHTML = renderGallery();
+
+new SimpleLightbox(".gallery a", {
+	captions: true,
+	captionsData: "alt",
+	captionPosition: "bottom",
+	captionDelay: 250,
+	overlayOpacity: 0.85,
+});
