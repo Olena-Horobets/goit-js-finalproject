f9df8b45b1511b850856097d65f60e9676a40793
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-01-06 19:29:05 -0330

    modal feature added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,7 +7,7 @@ const renderGallery = () => {
 	return galleryItems
 		.map((el) => {
 			return `<li class="gallery__item">
-                    <a class="gallery__link" href="large-image.jpg">
+                    <a class="gallery__link" href="${el.original}">
                         <img src="${el.preview}"
                             data-source="${el.original}"
                             alt="${el.description}" 
@@ -18,4 +18,23 @@ const renderGallery = () => {
 		.join("");
 };
 
+const galleryItemClickHandler = (e) => {
+	e.preventDefault();
+
+	if (e.target === e.currentTarget) return;
+
+	const img = e.target.closest("[href]");
+	const instance = basicLightbox.create(`
+	    <img src="${img}" width="800" height="600">
+	`);
+	const close = (e) => {
+		if (e.key === "Escape") instance.close();
+		document.removeEventListener("keydown", close);
+	};
+
+	instance.show();
+	document.addEventListener("keydown", close);
+};
+
 list.innerHTML = renderGallery();
+list.addEventListener("click", galleryItemClickHandler);
