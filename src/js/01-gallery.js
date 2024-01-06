b9c56ed8248251538e83660d62994134e9a1394a
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-01-06 17:26:59 -0330

    basic gallery render

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const renderGallery = () => {
+	return galleryItems
+		.map((el) => {
+			return `<li class="gallery__item">
+                    <a class="gallery__link" href="large-image.jpg">
+                        <img src="${el.preview}"
+                            data-source="${el.original}"
+                            alt="${el.description}" 
+                            class="gallery__image"/>
+                    </a>
+                </li>`;
+		})
+		.join("");
+};
+
+list.innerHTML = renderGallery();
